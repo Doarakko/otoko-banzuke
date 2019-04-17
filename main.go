a8a9a8cef0d169d1398ad3e6a166b70ebb08aa11
Doarakko <2wua4nlyi@gmail.com>
2019-04-17 17:01:30 +0900

    add form

--- main.go
+++ main.go
@@ -6,28 +6,41 @@ import (
 
 	"./youtube"
 	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	comments := youtube.SelectAllComments()
 
 	router := gin.Default()
 	router.Static("/public", "./public")
-	router.LoadHTMLGlob("templates/index.tmpl")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-	router.GET("/index", func(c *gin.Context) {
+	router.LoadHTMLFiles("templates/index.tmpl", "templates/form/index.tmpl")
+
+	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":    "Main website",
 			"comments": comments,
 		})
 	})
 
-	//router.POST("/index", SignupedRoute)
+	channels := []youtube.Channel{}
+	router.GET("/form", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "form/index.tmpl", gin.H{
+			"channels": channels,
+		})
+	})
+
+	router.POST("/form", func(c *gin.Context) {
+		query := c.PostForm("query")
+		channels = youtube.SearchChannel(query)
+		c.Redirect(302, "/form")
+	})
 
 	router.Run(":8080")
 }
