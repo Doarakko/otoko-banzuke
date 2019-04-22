a7db79b827d285be03fb24b3f5ec4e6a275e114a
Doarakko <2wua4nlyi@gmail.com>
2019-04-22 17:26:21 +0900

    add sample insert

--- table-layout/sql/comments.sql
+++ table-layout/sql/comments.sql
@@ -13,3 +13,7 @@ CREATE TABLE comments
 
     PRIMARY KEY (comment_id)
 );
+
+insert into comments
+    (comment_id, text_display, author_name, author_url, author_id, channel_id, video_id)
+values('test3', '本日の漢3', 'me', 'https://www.pokemon.co.jp/PostImages/99397d79bc49e82af280a2dcf9bbfdca23682273.jpg', 'test', 'test', 'test');
