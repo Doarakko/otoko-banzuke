bcd696739569ab9b9c6e3a18b1c7d67516b6d1ca
Doarakko <2wua4nlyi@gmail.com>
2019-04-22 17:21:08 +0900

    init internal commend

---
+++ internal/commend/main.go
@@ -0,0 +1,49 @@
+package commend
+
+import (
+	"log"
+
+	myyoutube "../../pkg/youtube"
+)
+
+// SearchChannels hoge
+func SearchChannels(q string) []myyoutube.Channel {
+	service := myyoutube.NewYoutubeService()
+	call := service.Search.List("id,snippet").
+		Type("channel").
+		Q(q).
+		Order("relevance").
+		MaxResults(10)
+	response, err := call.Do()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	channels := []myyoutube.Channel{}
+	for _, item := range response.Items {
+		channelID := item.Id.ChannelId
+		title := item.Snippet.Title
+		description := item.Snippet.Description
+		thumbnailURL := item.Snippet.Thumbnails.High.Url
+
+		channel := myyoutube.Channel{
+			ChannelID:    channelID,
+			Name:         title,
+			Description:  description,
+			ThumbnailURL: thumbnailURL,
+		}
+		channels = append(channels, channel)
+	}
+	log.Printf("Get %v channels\n", len(channels))
+
+	return channels
+}
+
+// InsertChannel get channel detail information and insert channel
+func InsertChannel(channelID string) {
+	channel := myyoutube.Channel{
+		ChannelID: channelID,
+	}
+	channel.SetDetailInfo()
+	channel.Insert()
+}
