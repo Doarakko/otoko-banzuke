7e7e41fd1e9661b6798566827793a1d23787d6b3
Doarakko <2wua4nlyi@gmail.com>
2019-04-22 15:21:09 +0900

    get rank and todays comment

--- pkg/youtube/comment.go
+++ pkg/youtube/comment.go
@@ -8,29 +8,24 @@ import (
 
 // Comment gaerg
 type Comment struct {
-	CommentID   string    `gorm:"column:comment_id"`
-	TextDisplay string    `gorm:"column:text_display"`
-	AuthorName  string    `gorm:"column:author_name"`
-	AuthorURL   string    `gorm:"column:author_url"`
-	LikeCount   int32     `gorm:"column:like_count"`
-	ReplyCount  int32     `gorm:"column:reply_count"`
-	ChannelID   string    `gorm:"column:channel_id"`
-	VideoID     string    `gorm:"column:video_id"`
-	CreatedAt   time.Time `gorm:"column:created_at"`
-	UpdatedAt   time.Time `gorm:"column:updated_at"`
+	CommentID    string    `gorm:"column:comment_id"`
+	TextDisplay  string    `gorm:"column:text_display"`
+	AuthorName   string    `gorm:"column:author_name"`
+	AuthorURL    string    `gorm:"column:author_url"`
+	LikeCount    int32     `gorm:"column:like_count"`
+	ReplyCount   int32     `gorm:"column:reply_count"`
+	ChannelID    string    `gorm:"column:channel_id"`
+	VideoID      string    `gorm:"column:video_id"`
+	CreatedAt    time.Time `gorm:"column:created_at"`
+	UpdatedAt    time.Time `gorm:"column:updated_at"`
+	Rank         int       `gorm:"-"`
+	ThumbnailURL string    `gorm:"-"`
 }
 
-var res = []*regexp.Regexp{
-	regexp.MustCompile("^.+男.{0,1}$"),
-	regexp.MustCompile("^.+漢.{0,1}$"),
-	regexp.MustCompile("^.+おとこ.{0,1}$"),
-	regexp.MustCompile("^.+オトコ.{0,1}$"),
-	regexp.MustCompile("^.+女.{0,1}$"),
-	regexp.MustCompile("^.+おんな.{0,1}$"),
-	regexp.MustCompile("^.+オンナ.{0,1}$"),
-}
+var re = regexp.MustCompile("^.+(男|漢|おとこ|オトコ|女|おんな|オンナ).{0,1}$")
 
-func (c *Comment) insert() {
+// Insert comment
+func (c *Comment) Insert() {
 	db := newGormConnect()
 	defer db.Close()
 
@@ -38,34 +33,44 @@ func (c *Comment) insert() {
 	log.Printf("Insert comment: %v\n", c)
 }
 
-func (c *Comment) update() {
+// Update comment
+func (c *Comment) Update() {
 
 }
 
-func (c *Comment) delete() {
+// Delete comment
+func (c *Comment) Delete() {
 
 }
 
-func (c *Comment) getCommentInfo() Comment {
-	return *c
-}
-
-func (c *Comment) checkOtoko() bool {
-	for _, re := range res {
-		if re.MatchString(c.TextDisplay) {
-			return true
-		}
+// CheckOtoko oge
+func (c *Comment) CheckOtoko() bool {
+	if re.MatchString(c.TextDisplay) {
+		return true
 	}
 	return false
 }
 
-// SelectAllComments get comments
-func SelectAllComments() []Comment {
+// SelectRankComments get comments
+func SelectRankComments() []Comment {
 	db := newGormConnect()
 	defer db.Close()
 
 	comments := []Comment{}
-	db.Find(&comments)
+	db.Select("*, RANK() OVER (ORDER BY comments.like_count DESC)").Order("rank").Joins("JOIN videos ON videos.video_id = comments.video_id").Find(&comments)
+
+	return comments
+}
+
+// SelectTodayComments get today comments
+func SelectTodayComments() []Comment {
+	db := newGormConnect()
+	defer db.Close()
+
+	comments := []Comment{}
+
+	preAt := time.Now().Add(-time.Duration(24 * time.Hour))
+	db.Where("created_at >= ?", preAt).Order("like_count desc").Find(&comments)
 
 	return comments
 }
