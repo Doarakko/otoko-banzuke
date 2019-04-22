cd32c573a0fc2f0aba8c45fa7c1d2509a3968760
Doarakko <2wua4nlyi@gmail.com>
2019-04-22 17:20:44 +0900

    init internal search

---
+++ internal/search/main.go
@@ -0,0 +1,22 @@
+package search
+
+import (
+	myyoutube "../../pkg/youtube"
+
+	mydb "../../pkg/database"
+)
+
+// SearchOtoko hoge
+func SearchOtoko(q string) []myyoutube.Comment {
+	db := mydb.NewGormConnect()
+	defer db.Close()
+
+	comments := []myyoutube.Comment{}
+	q = "%" + q + "%"
+	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url").
+		Where("text_display like ?", q).Joins("JOIN videos ON videos.video_id = comments.video_id").
+		Order("comments.like_count desc").
+		Find(&comments)
+
+	return comments
+}
