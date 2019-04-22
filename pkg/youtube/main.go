a308a88465bd3795f04ab84624defcc131b2441e
Doarakko <2wua4nlyi@gmail.com>
2019-04-22 17:19:55 +0900

    move newGormConnect to pkg/database

--- pkg/youtube/main.go
+++ pkg/youtube/main.go
@@ -5,21 +5,12 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/jinzhu/gorm"
 	"google.golang.org/api/googleapi/transport"
 	"google.golang.org/api/youtube/v3"
 )
 
-func newGormConnect() *gorm.DB {
-	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
-
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-func newYoutubeService() *youtube.Service {
+// NewYoutubeService hoge
+func NewYoutubeService() *youtube.Service {
 	client := &http.Client{
 		Transport: &transport.APIKey{Key: os.Getenv("YOUTUBE_API_KEY")},
 	}
