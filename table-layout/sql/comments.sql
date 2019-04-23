06ecf29b04a500ed16c8a92e1b5ed122391a3823
Doarakko <2wua4nlyi@gmail.com>
2019-04-23 14:38:02 +0900

    clean

--- table-layout/sql/comments.sql
+++ table-layout/sql/comments.sql
@@ -14,6 +14,6 @@ CREATE TABLE comments
     PRIMARY KEY (comment_id)
 );
 
-insert into comments
-    (comment_id, text_display, author_name, author_url, author_id, channel_id, video_id)
-values('test3', '本日の漢3', 'me', 'https://www.pokemon.co.jp/PostImages/99397d79bc49e82af280a2dcf9bbfdca23682273.jpg', 'test', 'test', 'test');
+-- insert into comments
+--     (comment_id, text_display, author_name, author_url, author_id, channel_id, video_id)
+-- values('test3', '本日の漢3', 'me', 'https://www.pokemon.co.jp/PostImages/99397d79bc49e82af280a2dcf9bbfdca23682273.jpg', 'test', 'test', 'test');
