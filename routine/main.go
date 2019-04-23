77046af6a975ef3998f45ec77ba83d3341b289c9
Doarakko <2wua4nlyi@gmail.com>
2019-04-23 17:29:30 +0900

    add few func

--- routine/main.go
+++ routine/main.go
@@ -1,27 +1,70 @@
 package main
 
 import (
+	"time"
+
+	mydb "github.com/Doarakko/otoko-banzuke/pkg/database"
 	myyoutube "github.com/Doarakko/otoko-banzuke/pkg/youtube"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+func selectAllChannels() []myyoutube.Channel {
+	db := mydb.NewGormConnect()
+	defer db.Close()
+
+	channels := []myyoutube.Channel{}
+	db.Find(&channels)
+
+	return channels
+}
+
+func selectNewChannels() []myyoutube.Channel {
+	db := mydb.NewGormConnect()
+	defer db.Close()
+
+	channels := []myyoutube.Channel{}
+
+	preAt := time.Now().Add(-time.Duration(24 * time.Hour))
+	db.Where("created_at >= ?", preAt).Find(&channels)
+
+	return channels
+}
 
 func updateAllComments() {
 
 }
 
-func main() {
-	// err := godotenv.Load("../../.env")
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+func updateAllChannels() {
 
-	for _, channel := range myyoutube.SelectAllChannels() {
+}
+
+func searchNewOtoko() {
+	for _, channel := range selectAllChannels() {
 		for _, video := range channel.GetNewVideos() {
 			for _, comment := range video.GetComments() {
 				if comment.CheckOtoko() {
+					video.Insert()
 					comment.Insert()
 				}
 			}
 		}
 	}
+}
+
+func searchAllOtoko() {
+	for _, channel := range selectAllChannels() {
+		for _, video := range channel.GetAllVideos("") {
+			for _, comment := range video.GetComments() {
+				if comment.CheckOtoko() {
+					video.Insert()
+					comment.Insert()
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	updateAllChannels()
+
 }
