d706a45878ff5d480182f133e219a78577a685a2
Doarakko <2wua4nlyi@gmail.com>
2019-04-24 19:49:56 +0900

    add limit

--- internal/banzuke/main.go
+++ internal/banzuke/main.go
@@ -15,8 +15,8 @@ func SelectRankComments() []myyoutube.Comment {
 	comments := []myyoutube.Comment{}
 	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url, RANK() OVER (ORDER BY comments.like_count DESC)").
 		Joins("JOIN videos ON videos.video_id = comments.video_id").
-		Where("comments.like_count >= ?", 10).
 		Order("rank").
+		Limit(50).
 		Find(&comments)
 
 	return comments
@@ -29,11 +29,14 @@ func SelectTodayComments() []myyoutube.Comment {
 
 	comments := []myyoutube.Comment{}
 
-	preAt := time.Now().Add(-time.Duration(24 * time.Hour))
+	// put 1 day period afer video published where get new video
+	preAt := time.Now().Add(-time.Duration(24*2) * time.Hour)
+
 	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url").
-		Where("comments.created_at >= ? and comments.like_count >= ?", preAt, 10).
+		Where("videos.published_at >= ?", preAt).
 		Joins("JOIN videos ON videos.video_id = comments.video_id").
-		Order("comments.created_at desc").
+		Order("comments.like_count DESC").
+		Limit(5).
 		Find(&comments)
 
 	return comments
