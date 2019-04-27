9a7131f4386f07ff8e6720d4424cb842d58989c3
Doarakko <2wua4nlyi@gmail.com>
2019-04-27 22:22:52 +0900

    init

---
+++ internal/base/main.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+	mydb "github.com/Doarakko/otoko-banzuke/pkg/database"
+)
+
+// GetTotalComment hoge
+func GetTotalComment() int {
+	db := mydb.NewGormConnect()
+	defer db.Close()
+
+	var count int
+	db.Table("comments").
+		Select("count(distinct(comments.comment_id))").
+		Count(&count)
+
+	return count
+}
+
+// GetTotalAuthor hoge
+func GetTotalAuthor() int {
+	db := mydb.NewGormConnect()
+	defer db.Close()
+
+	var count int
+	db.Table("comments").
+		Select("count(distinct(comments.author_url))").
+		Count(&count)
+
+	return count
+}
