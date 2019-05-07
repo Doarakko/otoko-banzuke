232dab29aca24d0e88e9a38289796cb43fcd1eec
Doarakko <2wua4nlyi@gmail.com>
2019-05-07 23:31:48 +0900

    add channel name and change func name

--- internal/search/main.go
+++ internal/search/main.go
@@ -5,14 +5,14 @@ import (
 	myyoutube "github.com/Doarakko/otoko-banzuke/pkg/youtube"
 )
 
-// SearchOtoko search comment related with parameter from database
-func SearchOtoko(q string) []myyoutube.Comment {
+// SearchComment search comment related with parameter from database
+func SearchComment(q string) []myyoutube.Comment {
 	db := mydb.NewGormConnect()
 	defer db.Close()
 
 	comments := []myyoutube.Comment{}
 	q = "%" + q + "%"
-	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url").
+	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url, channels.name").
 		Joins("JOIN videos ON videos.video_id = comments.video_id").
 		Joins("JOIN channels ON channels.channel_id = comments.channel_id").
 		Where("comments.text_display like ? or channels.name like ?", q, q).
