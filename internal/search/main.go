e66251791fe94bed05c57b496c9c66aca1753c27
Doarakko <2wua4nlyi@gmail.com>
2019-05-11 23:34:01 +0900

    search keyword convert to lower case

--- internal/search/main.go
+++ internal/search/main.go
@@ -1,6 +1,8 @@
 package search
 
 import (
+	"strings"
+
 	mydb "github.com/Doarakko/otoko-banzuke/pkg/database"
 	myyoutube "github.com/Doarakko/otoko-banzuke/pkg/youtube"
 )
@@ -11,11 +13,11 @@ func SearchComment(q string) []myyoutube.Comment {
 	defer db.Close()
 
 	comments := []myyoutube.Comment{}
-	q = "%" + q + "%"
-	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url, channels.name").
+	q = "%" + strings.ToLower(q) + "%"
+	db.Select("comments.text_display, comments.like_count, comments.video_id, comments.author_name, comments.author_url, videos.thumbnail_url, channels.name, comments.channel_id").
 		Joins("JOIN videos ON videos.video_id = comments.video_id").
 		Joins("JOIN channels ON channels.channel_id = comments.channel_id").
-		Where("comments.text_display like ? or channels.name like ? or videos.title like ?", q, q, q).
+		Where("lower(comments.text_display) like ? or lower(channels.name) like ? or lower(videos.title) like ?", q, q, q).
 		Order("comments.like_count desc").
 		Find(&comments)
 
